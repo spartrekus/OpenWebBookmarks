c8ade02afcb7aa9b821ad49432e3bcfec561028b
spartrekus <spartrekus@gmail.com>
2017-10-21 10:01:15 +0200

    Add files via upload

---
+++ OpenWebBookmarks/webview.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+ <title>Tiny AddUrl PHP</title>
+
+
+ 
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+
+<a href="index.html">[Home]</a>
+<a href="notepad.html">[View]</a>
+<br
+<hr/>
+ 
+
+
+<br/><br/>
+<br>
+<hr/>
+<?php
+//  This is a comment:
+//  Tiny PHP code for a notepad for education purpose and students (*easily hackable, no warranty!*) 
+include('notepad.html'); 
+?>
+<br>
+<hr/>
+ 
+
+
+</body>
+</html>
+
+
